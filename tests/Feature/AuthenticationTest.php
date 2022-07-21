8f7954e89d9958b222be08c8789daa0bca835226
Sean Anderson <wing5wong@hotmail.com>
2022-07-21 13:01:45 +1200

    cleanup tests

--- tests/Feature/AuthenticationTest.php
+++ tests/Feature/AuthenticationTest.php
@@ -12,65 +12,137 @@ class AuthenticationTest extends TestCase
     {
         $response = $this->postJson('/kamar');
 
-        $response->assertJson(['SMSDirectoryData' => ['error' => 403]]);
-        $response->assertJson(['SMSDirectoryData' => ['result' => 'Authentication Failed']]);
-        $response->assertJsonMissing(['SMSDirectoryData' => ['service' => config('kamar.serviceName')]]);
-        $response->assertJsonMissing(['SMSDirectoryData' => ['version' => config('kamar.serviceVersion')]]);
+        $response->assertJson([
+            'SMSDirectoryData' => [
+                'error' => 403,
+                'result' => 'Authentication Failed',
+            ]
+        ]);
+
+        $response->assertJsonMissing([
+            'SMSDirectoryData' => [
+                'service' => config('kamar.serviceName'),
+                'version' => config('kamar.serviceVersion')
+            ]
+        ]);
     }
 
     public function test_unauthenticated_check_request_returns_403_with_service_details()
     {
-        $response = $this->postJson('/kamar', ['SMSDirectoryData'=>['sync'=>'check']]);
+        $response = $this->postJson('/kamar', [
+            'SMSDirectoryData' => [
+                'sync' => 'check'
+            ]
+        ]);
 
-        $response->assertJson(['SMSDirectoryData' => ['error' => 403]]);
-        $response->assertJson(['SMSDirectoryData' => ['result' => 'Authentication Failed']]);
-        $response->assertJson(['SMSDirectoryData' => ['service' => config('kamar.serviceName')]]);
-        $response->assertJson(['SMSDirectoryData' => ['version' => config('kamar.serviceVersion')]]);
+        $response->assertJson([
+            'SMSDirectoryData' => [
+                'error' => 403,
+                'result' => 'Authentication Failed',
+                'service' => config('kamar.serviceName'),
+                'version' => config('kamar.serviceVersion'),
+            ]
+        ]);
     }
 
     public function test_invalid_credentials_returns_403()
     {
         $response = $this->withHeaders([
-            'HTTP_AUTHORIZATION' => "Basic " . base64_encode('Username' . ':' . 'Password'),
-        ])->postJson('/kamar');
+            'HTTP_AUTHORIZATION' => $this->invalidCredentials(),
+        ])
+            ->postJson('/kamar');
 
-        $response->assertJson(['SMSDirectoryData' => ['error' => 403]]);
+        $response->assertJson([
+            'SMSDirectoryData' => [
+                'error' => 403,
+                'result' => 'Authentication Failed',
+            ]
+        ]);
+
+        $response->assertJsonMissing([
+            'SMSDirectoryData' => [
+                'service' => config('kamar.serviceName'),
+                'version' => config('kamar.serviceVersion')
+            ]
+        ]);
     }
 
     public function test_authenticated_request_with_missing_data_returns_401()
     {
         $response = $this->withHeaders([
-            'HTTP_AUTHORIZATION' => "Basic " . base64_encode(config('kamar.username') . ':' . config('kamar.password')),
-        ])->postJson('/kamar');
+            'HTTP_AUTHORIZATION' => $this->validCredentials(),
+        ])
+            ->postJson('/kamar');
 
-        $response->assertJson(['SMSDirectoryData' => ['error' => 401]]);
-        $response->assertJson(['SMSDirectoryData' => ['result' => 'Missing Data']]);
-        $response->assertJsonMissing(['SMSDirectoryData' => ['service' => config('kamar.serviceName')]]);
-        $response->assertJsonMissing(['SMSDirectoryData' => ['version' => config('kamar.serviceVersion')]]);
+        $response->assertJson([
+            'SMSDirectoryData' => [
+                'error' => 401,
+                'result' => 'Missing Data',
+            ]
+        ]);
+
+        $response->assertJsonMissing([
+            'SMSDirectoryData' => [
+                'service' => config('kamar.serviceName'),
+                'version' => config('kamar.serviceVersion')
+            ]
+        ]);
     }
 
     public function test_authenticated_sync_check_request_with_data_returns_0_and_service()
     {
         $response = $this->withHeaders([
-            'HTTP_AUTHORIZATION' => "Basic " . base64_encode(config('kamar.username') . ':' . config('kamar.password')),
-        ])->postJson('/kamar', ['SMSDirectoryData'=>['sync'=>'check']]);
+            'HTTP_AUTHORIZATION' => $this->validCredentials(),
+        ])
+            ->postJson('/kamar', [
+                'SMSDirectoryData' => [
+                    'sync' => 'check'
+                ]
+            ]);
 
-        $response->assertJson(['SMSDirectoryData' => ['error' => 0]]);
-        $response->assertJson(['SMSDirectoryData' => ['result' => 'OK']]);
-        $response->assertJson(['SMSDirectoryData' => ['service' => config('kamar.serviceName')]]);
-        $response->assertJson(['SMSDirectoryData' => ['version' => config('kamar.serviceVersion')]]);
+        $response->assertJson([
+            'SMSDirectoryData' => [
+                'error' => 0,
+                'result' => 'OK',
+                'service' => config('kamar.serviceName'),
+                'version' => config('kamar.serviceVersion'),
+            ]
+        ]);
     }
 
     public function test_authenticated_request_with_data_returns_0_and_no_service_details()
     {
         $response = $this->withHeaders([
-            'HTTP_AUTHORIZATION' => "Basic " . base64_encode(config('kamar.username') . ':' . config('kamar.password')),
-        ])->postJson('/kamar', ['SMSDirectoryData'=>['sync'=>'part']]);
+            'HTTP_AUTHORIZATION' => $this->validCredentials(),
+        ])
+            ->postJson('/kamar', [
+                'SMSDirectoryData' => [
+                    'sync' => 'part'
+                ]
+            ]);
 
-        $response->assertJson(['SMSDirectoryData' => ['error' => 0]]);
-        $response->assertJson(['SMSDirectoryData' => ['result' => 'OK']]);
-        $response->assertJsonMissing(['SMSDirectoryData' => ['service' => config('kamar.serviceName')]]);
-        $response->assertJsonMissing(['SMSDirectoryData' => ['version' => config('kamar.serviceVersion')]]);
+        $response->assertJson([
+            'SMSDirectoryData' => [
+                'error' => 0,
+                'result' => 'OK',
+            ]
+        ]);
+
+        $response->assertJsonMissing([
+            'SMSDirectoryData' => [
+                'service' => config('kamar.serviceName'),
+                'version' => config('kamar.serviceVersion')
+            ]
+        ]);
     }
 
+    private function validCredentials()
+    {
+        return "Basic " . base64_encode(config('kamar.username') . ':' . config('kamar.password'));
+    }
+
+    private function invalidCredentials()
+    {
+        return "Basic " . base64_encode('username' . ':' . 'password');
+    }
 }
