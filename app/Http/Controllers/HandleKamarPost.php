01e2913d2ca87f27187676cf4256f24e6271c6d7
Sean Anderson <wing5wong@hotmail.com>
2022-07-21 16:15:03 +1200

    refactor HandleKamarPost

--- app/Http/Controllers/HandleKamarPost.php
+++ app/Http/Controllers/HandleKamarPost.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Responses\Check\{Success as CheckSuccess, FailedAuthentication as CheckFailedAuthentication, MissingData as CheckMissingData};
+use App\Responses\Check\{Success as CheckSuccess, FailedAuthentication as CheckFailedAuthentication};
 use App\Responses\Standard\{Success, FailedAuthentication, MissingData};
 use App\{AuthenticationCheck, KamarData};
 
@@ -18,21 +18,12 @@ class HandleKamarPost extends Controller
     public function __invoke()
     {
         // Check supplied username/password  matches our expectation
-        if ($this->data->isSyncCheck()) {
-
-            if ($this->authCheck->fails()) {
-                return response()->json(new CheckFailedAuthentication());  // failed during check, we have to return error, result, version and service
-            }
-
-            if ($this->data->isMissing()) {
-                return response()->json(new CheckMissingData());
-            }
-
-            return response()->json(new CheckSuccess());
-        }
-
         if ($this->authCheck->fails()) {
-            return response()->json(new FailedAuthentication());  // failed during any other request, we only have to return error and result
+            if ($this->data->isSyncCheck()) {
+                return response()->json(new CheckFailedAuthentication());  // failed during check, we have to return error, result, version and service
+            } else {
+                return response()->json(new FailedAuthentication());  // failed during any other request, we only have to return error and result
+            }
         }
 
         // Check we have some data
@@ -40,9 +31,9 @@ class HandleKamarPost extends Controller
             return response()->json(new MissingData());
         }
 
-        // Check if a 'part' sync, return part response.
-        if ($this->data->isSyncPart()) {
-            return $this->handleOKResponse();
+        // Check if a 'check' sync, return check response.
+        if ($this->data->isSyncCheck()) {
+            return response()->json(new CheckSuccess());
         }
 
         // All other messages - store the data and return 'OK' response.
--- app/Responses/Check/MissingData.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Responses\Check;
-
-class MissingData extends AbstractCheckResponse
-{
-    protected int $error = 401;
-    protected string $result = "Missing Data";
-}
