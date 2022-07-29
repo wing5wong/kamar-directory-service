4c4b0a5076bf7e53de370278e9570ab3973b7629
Sean Anderson <wing5wong@hotmail.com>
2022-07-29 16:16:22 +1200

    add factory methods to KamarData Allow generating from request or file

--- app/KamarData.php
+++ app/KamarData.php
@@ -2,19 +2,22 @@
 
 namespace App;
 
+use Exception;
+use App\Jobs\ProcessKamarPost;
 use Illuminate\Support\Facades\Storage;
 
 class KamarData
 {
+    public $data;
 
     public function isMissing()
     {
-        return !request()->has('SMSDirectoryData');
+        return empty(data_get($this->data,'SMSDirectoryData'));
     }
 
     public function getSyncType()
     {
-        return request('SMSDirectoryData.sync');
+        return data_get($this->data, 'SMSDirectoryData.sync');
     }
 
     public function isSyncCheck()
@@ -24,13 +27,29 @@ class KamarData
 
     public function isSyncPart()
     {
+        if(!$this->data) { return false; }
         return $this->getSyncType() === "part";
     }
 
     public function store()
     {
-        Storage::disk('local')
-            ->put('data/' . $this->getSyncType() . "_" . time() . "_" . mt_rand(1000, 9999) . ".json", request()->getContent());
+        $filename = $this->getSyncType() . "_" . time() . "_" . mt_rand(1000, 9999) . ".json";
+        Storage::put('data/' . $filename, request()->getContent());
+        ProcessKamarPost::dispatch($filename);
+    }
+
+    public static function fromRequest()
+    {
+        $kamarData = new static;
+        $kamarData->data = collect(request()->input());
+        return $kamarData;
+    }
+
+    public static function fromFile($filename)
+    {        
+        $kamarData = new static;
+        $kamarData->data = collect(json_decode(Storage::disk('local')->get('data/' . $filename), true));
+        return $kamarData;
     }
 
 }
--- tests/Unit/KamarDataTest.php
+++ tests/Unit/KamarDataTest.php
@@ -17,51 +17,55 @@ class KamarDataTest extends TestCase
     public function test_isMissing_returns_true_when_request_SMSDirectoryData_is_empty_array()
     {
         $this->setupEmptyRequest();
-        $this->assertFalse((new KamarData())->isMissing());
+        $this->assertTrue((KamarData::fromRequest())->isMissing());
     }
 
     public function test_isSyncCheck_returns_true_when_sync_is_check()
     {
         $this->setupSyncCheckRequest();
-        $this->assertTrue((new KamarData())->isSyncCheck());
+        $kamar = KamarData::fromRequest();
+        $this->assertTrue($kamar->isSyncCheck());
     }
 
     public function test_isSyncCheck_returns_false_when_sync_is_not_check()
     {
         $this->setupSyncPartRequest();
-        $this->assertFalse((new KamarData())->isSyncCheck());
+        $kamar = KamarData::fromRequest(request());
+        $this->assertFalse($kamar->isSyncCheck());
     }
 
     public function test_isSyncPart_returns_true_when_sync_is_part()
     {
         $this->setupSyncPartRequest();
-        $this->assertTrue((new KamarData())->isSyncPart());
+        $kamar = KamarData::fromRequest(request());
+        $this->assertTrue($kamar->isSyncPart());
     }
 
     public function test_isSyncPart_returns_false_when_sync_is_not_part()
     {
         $this->setupSyncCheckRequest();
-        $this->assertFalse((new KamarData())->isSyncPart());
+        $kamar = KamarData::fromRequest(request());
+        $this->assertFalse($kamar->isSyncPart());
     }
 
     private function setupSyncCheckRequest()
     {
         $request = new Request();
-        $request->replace(['SMSDirectoryData' => ['sync' => 'check']]);
+        $request->merge(['SMSDirectoryData' => ['sync' => 'check']]);
         app()->instance('request', $request);
     }
 
     private function setupSyncPartRequest()
     {
         $request = new Request();
-        $request->replace(['SMSDirectoryData' => ['sync' => 'part']]);
+        $request->merge(['SMSDirectoryData' => ['sync' => 'part']]);
         app()->instance('request', $request);
     }
 
     private function setupEmptyRequest()
     {
         $request = new Request();
-        $request->replace(['SMSDirectoryData' => []]);
+        $request->merge(['SMSDirectoryData' => []]);
         app()->instance('request', $request);
     }
 
